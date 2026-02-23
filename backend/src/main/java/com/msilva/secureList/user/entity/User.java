d3a4913a263f493c57883303a2a6f4af571eb926
Matheus Silva <mathsilva.code@gmail.com>
2026-02-23 20:18:56 -0300

    feat: add constructor to User

--- backend/src/main/java/com/msilva/secureList/user/entity/User.java
+++ backend/src/main/java/com/msilva/secureList/user/entity/User.java
@@ -40,4 +40,13 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<Role> roles;
+
+    public User(String email, String username,
+                String password, Set<Role> roles){
+
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
 }
