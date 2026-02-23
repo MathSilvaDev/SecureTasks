613b25daabd9e08791507cbf9196380e3955926a
Matheus Silva <mathsilva.code@gmail.com>
2026-02-23 20:01:23 -0300

    feat: add UserRepository interface

---
+++ backend/src/main/java/com/msilva/secureList/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.msilva.secureList.user.repository;
+
+import com.msilva.secureList.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
