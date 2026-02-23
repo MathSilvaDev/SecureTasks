94944c17785c1bcdb96532fcb599a3edf10721be
Matheus Silva <mathsilva.code@gmail.com>
2026-02-23 19:49:13 -0300

    chore: initial backend setup with project structure

---
+++ backend/src/main/java/com/msilva/secureList/SecureListApplication.java
@@ -0,0 +1,13 @@
+package com.msilva.secureList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureListApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/msilva/secureList/SecureListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msilva.secureList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
