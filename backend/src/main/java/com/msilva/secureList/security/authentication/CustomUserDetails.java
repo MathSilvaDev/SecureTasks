600062f4120d92268986d3cd7298104e409341ec
Matheus Silva <mathsilva.code@gmail.com>
2026-02-24 15:12:53 -0300

    feat: implement CustomUserDetails for authentication

---
+++ backend/src/main/java/com/msilva/secureList/security/authentication/CustomUserDetails.java
@@ -0,0 +1,66 @@
+package com.msilva.secureList.security.authentication;
+
+import com.msilva.secureList.user.entity.User;
+import lombok.Getter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+public class CustomUserDetails implements UserDetails {
+
+    @Getter
+    private UUID id;
+
+    private String email;
+    private String password;
+    private List<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(User user){
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                .toList();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
