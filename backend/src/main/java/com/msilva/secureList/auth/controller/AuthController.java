77e677a5797f4aa72354c576e1b29ea32104da85
Matheus Silva <mathsilva.code@gmail.com>
2026-02-25 15:09:23 -0300

    feat: add AuthController and AuthService base structure

---
+++ backend/src/main/java/com/msilva/secureList/auth/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.msilva.secureList.auth.controller;
+
+import com.msilva.secureList.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+}
