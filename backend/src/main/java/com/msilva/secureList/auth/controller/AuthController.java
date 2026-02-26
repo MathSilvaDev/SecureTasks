405ff4b9b034c1724493da40bb3acd4f30b7ed17
Matheus Silva <mathsilva.code@gmail.com>
2026-02-26 16:46:20 -0300

    feat(auth): implement register endpoint in controller and service

--- backend/src/main/java/com/msilva/secureList/auth/controller/AuthController.java
+++ backend/src/main/java/com/msilva/secureList/auth/controller/AuthController.java
@@ -1,7 +1,12 @@
 package com.msilva.secureList.auth.controller;
 
+import com.msilva.secureList.auth.dto.request.RegisterRequest;
 import com.msilva.secureList.auth.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(
+            @Valid @RequestBody RegisterRequest request){
+
+        authService.register(request);
+        return ResponseEntity.noContent().build();
+    }
 }
--- backend/src/main/java/com/msilva/secureList/auth/service/AuthService.java
+++ backend/src/main/java/com/msilva/secureList/auth/service/AuthService.java
@@ -1,9 +1,47 @@
 package com.msilva.secureList.auth.service;
 
+import com.msilva.secureList.auth.dto.request.RegisterRequest;
+import com.msilva.secureList.role.entity.Role;
+import com.msilva.secureList.role.enums.RoleValues;
+import com.msilva.secureList.role.repository.RoleRepository;
+import com.msilva.secureList.user.entity.User;
+import com.msilva.secureList.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void register(RegisterRequest request){
+
+        if(userRepository.existsByEmail(request.email())){
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+
+        Role roleBasic = roleRepository.findByName(RoleValues.BASIC.name()).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        String hashPassword = passwordEncoder.encode(request.password());
+
+        User user = new User(
+                request.email(),
+                request.username(),
+                hashPassword,
+                Set.of(roleBasic)
+        );
+
+        userRepository.save(user);
+    }
 }
