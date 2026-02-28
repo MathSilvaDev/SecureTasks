a2153505814cec8de9feef9b7acff1c8974cf6f5
Matheus Silva <mathsilva.code@gmail.com>
2026-02-28 19:39:01 -0300

    feat(register): implement register logic with AuthService integration and redirect to login

--- frontend/src/app/register/register.ts
+++ frontend/src/app/register/register.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { AuthService } from '../auth/auth.service';
+import { NavigateService } from '../navigate.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
 
+  email: string = '';
+  username: string = '';
+  password: string = '';
+
+  constructor(
+    private authService: AuthService,
+    private navigateService: NavigateService
+  ) {}
+
+  register() {
+    const email = this.email.trim();
+    const username = this.username.trim();
+    const password = this.password.trim();
+
+    this.authService.register(email, username, password).subscribe({
+      next: (response) => {
+        console.log('Registration successful:', response);
+        this.navigateService.goToLogin();
+      },
+      error: (err) => {
+        console.error('Registration failed:', err);
+      } 
+    })
+  }
+
+  
 }
