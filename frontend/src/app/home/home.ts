c2c82c302be684cb94e47284cb1780428366fa69
Matheus Silva <mathsilva.code@gmail.com>
2026-02-28 20:25:37 -0300

    feat(home): implement tasks UI with form and list structure and fix routes after login

---
+++ frontend/src/app/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  imports: [FormsModule],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+
+  title: string = '';
+  description: string = '';
+}
