df953f2b9db9084b7a2973045d622bad07ea3f39
Matheus Silva <mathsilva.code@gmail.com>
2026-03-01 18:10:23 -0300

    feat(task): create Controller and Service

---
+++ backend/src/main/java/com/msilva/secureList/task/controller/TaskController.java
@@ -0,0 +1,14 @@
+package com.msilva.secureList.task.controller;
+
+import com.msilva.secureList.task.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final TaskService taskService;
+}
---
+++ backend/src/main/java/com/msilva/secureList/task/service/TaskService.java
@@ -0,0 +1,9 @@
+package com.msilva.secureList.task.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+}
