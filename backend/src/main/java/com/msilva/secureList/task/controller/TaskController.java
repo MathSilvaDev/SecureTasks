09c9e748daff82c211d1a335fdfeb03562b2840b
Matheus Silva <mathsilva.code@gmail.com>
2026-03-01 18:21:51 -0300

    feat(task): implement findAllByUser endpoint and service logic

--- backend/src/main/java/com/msilva/secureList/task/controller/TaskController.java
+++ backend/src/main/java/com/msilva/secureList/task/controller/TaskController.java
@@ -1,9 +1,15 @@
 package com.msilva.secureList.task.controller;
 
+import com.msilva.secureList.task.dto.response.TaskResponse;
 import com.msilva.secureList.task.service.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -11,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaskController {
 
     private final TaskService taskService;
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponse>> findAllByUser(Authentication auth){
+        return ResponseEntity
+                .ok(taskService.findAllByUser(auth.getName()));
+    }
 }
--- backend/src/main/java/com/msilva/secureList/task/service/TaskService.java
+++ backend/src/main/java/com/msilva/secureList/task/service/TaskService.java
@@ -1,9 +1,44 @@
 package com.msilva.secureList.task.service;
 
+import com.msilva.secureList.task.dto.response.TaskResponse;
+import com.msilva.secureList.task.entity.Task;
+import com.msilva.secureList.task.repository.TaskRepository;
+import com.msilva.secureList.user.entity.User;
+import com.msilva.secureList.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
+
+    public List<TaskResponse> findAllByUser(String authEmail){
+        User user = findUserByEmail(authEmail);
+
+        return taskRepository.findAllByUser(user)
+                .stream()
+                .map(this::toResponse)
+                .toList();
+    }
+
+    private TaskResponse toResponse(Task task){
+        return new TaskResponse(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.isCompleted(),
+                task.getCreatedAt()
+        );
+    }
+
+    private User findUserByEmail(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not Found"));
+    }
 }
